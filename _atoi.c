f0d7514ff8afd4d6012553302db637c7074990a5
youssef el ouidad <y.elouidad@gmail.com>
2023-07-21 00:07:49 +0100

    my second commit in S shell

---
+++ _atoi.c
@@ -0,0 +1,84 @@
+#include "shell.h"
+
+/**
+ * interactive - the function checks if shell is running in interactive mode
+ * @info: struct address
+ *
+ * Return: 1 if shell is in interactive mode, 0 otherwise
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * _isalpha: checks for alphabetic characters.
+ * if the input is character it  showes: 1
+ * otherwise it shows 0
+ * @c: the character in ASCII code
+ * Return: 1 for alphabetic charracter 0 for the rest.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
+
+/**
+ * is_delim - checks if character is a delimiter
+ * @c: the char to check
+ * @delim: the delimiter string
+ * Return: 1 if true, 0 if false
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+	{
+		if (*delim == c)
+			return (1);
+		delim++;
+	}
+	return (0);
+}
+
+/**
+ * _atoi - a function that converts a string to an integer
+ * @s: the string to be converted
+ *
+ * Return: the converted integer value
+ */
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
+
